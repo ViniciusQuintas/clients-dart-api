b8ba3cfd7133e201ea0f06bb5a21dcf854574343
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-30 16:47:27 -0300

    feat: add imports handlers

--- lib/app/api/api.dart
+++ lib/app/api/api.dart
@@ -1,3 +1,6 @@
 library api;
 
 part "controllers/clientes/clientes_controller.dart";
+part 'controllers/controller.dart';
+part 'handlers/handler.dart';
+part 'handlers/clientes/get_clientes_dandler.dart';

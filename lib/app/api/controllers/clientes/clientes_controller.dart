d179986998db46fc0cf813620f829bf34032a29c
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-30 16:35:11 -0300

    feat: create class GetClientesHandler

--- lib/app/api/controllers/clientes/clientes_controller.dart
+++ lib/app/api/controllers/clientes/clientes_controller.dart
@@ -1,14 +1,20 @@
 part of api;
 
+class GetClienteshandler {}
+
 abstract class Controller {
   final String route;
+  final Map<String, GetClienteshandler> handlers;
 
-  Controller({
-    required this.route,
-  });
+  Controller({required this.route, required this.handlers});
 }
 
 class ClientesController implements Controller {
   @override
   String get route => '/clientes';
+
+  @override
+  Map<String, GetClienteshandler> get handlers => {
+        'GET': GetClienteshandler(),
+      };
 }

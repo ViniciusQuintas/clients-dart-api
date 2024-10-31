55768d0931c6523ec94f43ff260f4b412213781b
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 11:40:50 -0300

    refactor: update controller.dart

--- lib/app/api/controllers/controller.dart
+++ lib/app/api/controllers/controller.dart
@@ -3,6 +3,8 @@ part of api;
 abstract class Controller {
   final String route;
   final Map<String, Handler> handlers;
-
-  Controller({required this.route, required this.handlers});
+  Controller({
+    required this.route,
+    required this.handlers,
+  });
 }

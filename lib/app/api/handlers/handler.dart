02122e5434f78fdc72ea60a7e865ec0133f67491
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 11:40:51 -0300

    refactor: update handler.dart

--- lib/app/api/handlers/handler.dart
+++ lib/app/api/handlers/handler.dart
@@ -1,16 +1,31 @@
 part of api;
 
-class ResponseHandler {
+class ResponseHandler<T> {
   final StatusHandler status;
-  final Object body;
+  final T? body;
 
-  ResponseHandler({required this.status, required this.body});
+  ResponseHandler({
+    required this.status,
+    this.body,
+  });
+}
+
+class RequestParams {
+  final Map<String, dynamic>? body;
+  final Map<String, dynamic>? path;
+
+  RequestParams({this.body, this.path});
 }
 
 enum StatusHandler {
   ok,
+  internalServerError,
+  created,
+  badRequest,
+  noContent,
+  notFound,
 }
 
 abstract class Handler {
-  Future<ResponseHandler> call();
+  Future<ResponseHandler> call(RequestParams requestParams);
 }

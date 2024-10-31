5c23e001978a1ce4994395aa1eada9528008b5b0
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 11:40:56 -0300

    feat: add config

---
+++ lib/config/config.dart
@@ -0,0 +1,14 @@
+library config;
+
+import 'package:clients_dart_api/app/api/api.dart';
+import 'package:clients_dart_api/app/data/data.dart';
+import 'package:clients_dart_api/app/domain/domain.dart';
+import 'package:clients_dart_api/app/infra/infra.dart';
+import 'package:dotenv/dotenv.dart';
+import 'package:get_it/get_it.dart';
+
+part 'main/main.dart';
+part 'di/di.dart';
+part 'env/env.dart';
+
+part 'di/clientes_inject.dart';

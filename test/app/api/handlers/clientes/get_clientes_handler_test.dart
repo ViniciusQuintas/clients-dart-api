77e257962fd25555e0e87d8115ccf2a86e9b03df
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 08:05:11 -0300

    test: implements  getUsecasesClientes

--- test/app/api/handlers/clientes/get_clientes_handler_test.dart
+++ test/app/api/handlers/clientes/get_clientes_handler_test.dart
@@ -1,23 +1,29 @@
 import 'package:clients_dart_api/app/api/api.dart';
 import 'package:test/test.dart';
 
+import '../../../../mock.dart';
+
 void main() {
+  late GetClienteshandler handler;
+  late MockGetClientesUseCase mockGetClientesUseCase;
+
+  setUpAll(() {
+    mockGetClientesUseCase = MockGetClientesUseCase();
+    handler = GetClienteshandler(getClientesUseCase: mockGetClientesUseCase);
+  });
   test('deve retornar uma instancia  de ResponseHandler ...', () async {
-    final handler = GetClienteshandler();
     final result = await handler.call();
 
     expect(result, isA<ResponseHandler>());
   });
 
   test('deve retornar uma status ok ...', () async {
-    final handler = GetClienteshandler();
     final result = await handler.call();
 
     expect(result.status, StatusHandler.ok);
   });
 
   test('deve retornar uma lista de ClienteOutputDTO ...', () async {
-    final handler = GetClienteshandler();
     final result = await handler.call();
 
     expect(result.body, isA<List<ClienteOutputDto>>());

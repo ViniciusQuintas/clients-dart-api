4d1980da9f9542db71d92b35bb305649b9dc852f
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 11:40:51 -0300

    refactor: update cliente_output_dto.dart

--- lib/app/api/dto/cliente_output_dto.dart
+++ lib/app/api/dto/cliente_output_dto.dart
@@ -1,14 +1,36 @@
 part of api;
 
-class ClienteOutputDto {
+class ClienteOutputDTO {
   final int id;
   final String nome;
   final String email;
   final String telefone;
 
-  ClienteOutputDto(
-      {required this.id,
-      required this.nome,
-      required this.email,
-      required this.telefone});
+  ClienteOutputDTO({
+    required this.id,
+    required this.nome,
+    required this.email,
+    required this.telefone,
+  });
+
+  factory ClienteOutputDTO.toDTO(Cliente cliente) {
+    return ClienteOutputDTO(
+        id: cliente.id,
+        nome: cliente.nome,
+        email: cliente.email,
+        telefone: cliente.telefone);
+  }
+
+  static List<ClienteOutputDTO> toCollectionDTO(List<Cliente> clientes) {
+    return clientes.map(ClienteOutputDTO.toDTO).toList();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nome': nome,
+      'email': email,
+      'telefone': telefone,
+    };
+  }
 }

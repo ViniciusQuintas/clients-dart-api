cf370f95ad8d3f3686f3f8957c0f6a32e2aed793
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-30 23:45:45 -0300

    feat: implement async function call as return Responsehandler

--- lib/app/api/handlers/clientes/get_clientes_handler.dart
+++ lib/app/api/handlers/clientes/get_clientes_handler.dart
@@ -1,3 +1,9 @@
 part of api;
 
-class GetClienteshandler implements Handler {}
+class GetClienteshandler implements Handler {
+  @override
+  Future<ResponseHandler> call() async {
+    return ResponseHandler(
+        status: StatusHandler.ok, body: <ClienteOutputDto>[]);
+  }
+}

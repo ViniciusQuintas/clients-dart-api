492fb2b098c970f2ec6821600c6b6df645de2693
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 08:08:50 -0300

    test: implements test for use case controller

--- test/app/api/controllers/clientes/clientes_controller_test.dart
+++ test/app/api/controllers/clientes/clientes_controller_test.dart
@@ -1,11 +1,16 @@
 import 'package:clients_dart_api/app/api/api.dart';
 import 'package:test/test.dart';
 
+import '../../../../mock.dart';
+
 void main() {
   late ClientesController controller;
+  late MockGetClientesUseCase mockGetClientesUseCase;
 
   setUpAll(() {
-    controller = ClientesController();
+    mockGetClientesUseCase = MockGetClientesUseCase();
+    controller =
+        ClientesController(getClientesUseCases: mockGetClientesUseCase);
   });
 
   group('ClientesController:', () {

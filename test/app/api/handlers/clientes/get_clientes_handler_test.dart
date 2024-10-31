b672c11e6e4ec62997d5934fed9d72e86ca4fa21
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 11:40:52 -0300

    test: update get_clientes_handler_test.dart

--- test/app/api/handlers/clientes/get_clientes_handler_test.dart
+++ test/app/api/handlers/clientes/get_clientes_handler_test.dart
@@ -1,31 +1,49 @@
 import 'package:clients_dart_api/app/api/api.dart';
+import 'package:clients_dart_api/app/domain/domain.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 import '../../../../mock.dart';
 
 void main() {
-  late GetClienteshandler handler;
+  late GetClientesHandler handler;
   late MockGetClientesUseCase mockGetClientesUseCase;
 
   setUpAll(() {
     mockGetClientesUseCase = MockGetClientesUseCase();
-    handler = GetClienteshandler(getClientesUseCase: mockGetClientesUseCase);
-  });
-  test('deve retornar uma instancia  de ResponseHandler ...', () async {
-    final result = await handler.call();
-
-    expect(result, isA<ResponseHandler>());
+    handler = GetClientesHandler(getClientesUseCase: mockGetClientesUseCase);
   });
 
-  test('deve retornar uma status ok ...', () async {
-    final result = await handler.call();
+  group('GetClientesHandler:', () {
+    test('deve retornar uma instancia de ResponseHandler', () async {
+      when(() => mockGetClientesUseCase.call()).thenAnswer((_) async => []);
+      final result = await handler.call(RequestParams());
 
-    expect(result.status, StatusHandler.ok);
-  });
+      expect(result, isA<ResponseHandler>());
+    });
 
-  test('deve retornar uma lista de ClienteOutputDTO ...', () async {
-    final result = await handler.call();
+    test('deve retornar um status ok', () async {
+      when(() => mockGetClientesUseCase.call()).thenAnswer((_) async => []);
+      final result = await handler.call(RequestParams());
 
-    expect(result.body, isA<List<ClienteOutputDto>>());
+      expect(result.status, StatusHandler.ok);
+    });
+
+    test('deve retornar uma lista de ClienteOutputDTO', () async {
+      when(() => mockGetClientesUseCase.call()).thenAnswer((_) async =>
+          [Cliente(id: 1, nome: 'nome', email: 'email', telefone: 'telefone')]);
+      final result = await handler.call(RequestParams());
+
+      expect(result.body, isA<List<ClienteOutputDTO>>());
+    });
+
+    test(
+        'deve retornar um status internalServerError quando um erro desconhecido acontencer',
+        () async {
+      when(() => mockGetClientesUseCase.call()).thenThrow(Exception());
+      final result = await handler.call(RequestParams());
+
+      expect(result.status, StatusHandler.internalServerError);
+    });
   });
 }

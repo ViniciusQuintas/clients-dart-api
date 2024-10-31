a53ff023f47f16929492b6acb47a74140a61798d
ViniciusQuintas <viniciusquintas744@gmail.com>
2024-10-31 08:07:55 -0300

    feat: implements clientes use cases

--- lib/app/api/handlers/clientes/get_clientes_handler.dart
+++ lib/app/api/handlers/clientes/get_clientes_handler.dart
@@ -1,9 +1,14 @@
 part of api;
 
 class GetClienteshandler implements Handler {
+  final GetClientesUseCases getClientesUseCase;
+
+  GetClienteshandler({required this.getClientesUseCase});
   @override
   Future<ResponseHandler> call() async {
     return ResponseHandler(
         status: StatusHandler.ok, body: <ClienteOutputDto>[]);
   }
 }
+
+class GetClientesUseCases {}
